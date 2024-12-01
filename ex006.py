8bc6564402cca444ccde86472d9c5ea2bbf8eaf5
GarciaRPedro <pedroxzz2003@gmail.com>
2024-12-01 13:21:37 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -4,3 +4,5 @@
 #           seu dobro, triplo e quadrado
 #
 #
+num1 = int(input('Digite um número inteiro: '))
+print('O dobro de {} é: {}\nSeu triplo é : {}\nE seu quadrado é: {}'.format(num1, num1*2, num1*3, num1**2))
