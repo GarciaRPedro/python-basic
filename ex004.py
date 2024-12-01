78af432fd1f329ce03a8553b8ea3ae10f42f0e2e
GarciaRPedro <pedroxzz2003@gmail.com>
2024-12-01 01:16:04 -0300

    Update ex004.py

--- ex004.py
+++ ex004.py
@@ -1,3 +1,9 @@
 #
 #       Algoritmo para "dissecar" uma variavel
 #
+n1 = input('Digite alguma coisa: ')
+print('O valor digitado é do tipo: ',type(n1))
+print(',É uma letra ou palavra ?',n1.isalpha())
+print(',É composto só de espaços ?', n1.isspace())
+print(',É numérico ?',n1.isnumeric())
+print(',É alfanumerico ?',n1.isalnum())
