c10e9c16289cb903a7867f74636593802093a755
GarciaRPedro <pedroxzz2003@gmail.com>
2024-12-01 13:13:07 -0300

    Exercicio
    exercicio 01 da aula 07

---
+++ ex005.py
@@ -0,0 +1,9 @@
+#
+#
+#       Algoritmo que lê um numero interio e exiba na tela
+#           seu antecessor e seu sucessor
+#
+#
+
+num1 = int(input('Digite um número inteiro: '))
+print('O antessesor de "{}" é: {}\nE o seu suscessor é: {}'.format(num1, num1-1, num1+1))
