0dbe3012e3128cb4da4fddab96acf78f0b9adcbc
GarciaRPedro <pedroxzz2003@gmail.com>
2024-12-01 16:53:09 -0300

    Create ex007.py

---
+++ ex007.py
@@ -0,0 +1,10 @@
+#
+#
+#       Algoritmo que leia duas notas de um aluno e exiba na tela
+#           a sua média geral
+#
+#
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+print('A média obtida é: {}'.format(media))
