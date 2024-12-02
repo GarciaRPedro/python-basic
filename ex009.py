cf199ad8d19261a24b53b2eb3d35a0fe53bef10c
GarciaRPedro <pedroxzz2003@gmail.com>
2024-12-01 22:12:47 -0300

    Update ex009.py

--- ex009.py
+++ ex009.py
@@ -5,4 +5,15 @@
 #
 #
 n = int(input('Digite um numero inteiro: '))
-print('')
+print('-'*12)
+print('{} x  1 = {}'.format(n, n*1))
+print('{} x  2 = {}'.format(n, n*2))
+print('{} x  3 = {}'.format(n, n*3))
+print('{} X  4 = {}'.format(n, n*4))
+print('{} X  5 = {}'.format(n, n*5))
+print('{} x  6 = {}'.format(n, n*6))
+print('{} x  7 = {}'.format(n, n*7))
+print('{} x  8 = {}'.format(n, n*8))
+print('{} x  9 = {}'.format(n, n*9))
+print('{} x 10 = {}'.format(n, n*10))
+print('-'*12)
