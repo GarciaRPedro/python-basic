2d601e8b4720b12d291946870f86cbf7bb36d911
GarciaRPedro <pedroxzz2003@gmail.com>
2024-12-03 10:16:16 -0300

    Ex

---
+++ ex013.py
@@ -0,0 +1,8 @@
+#
+#
+#                   Algoritmo que leia a o Salario de um funcionario e exiba na tela
+#                                 com reajuste de 15% de promoçãp
+#
+#
+salario = float(input('Digite o valor que receberá o reajuste R$: '))
+print('O valor de {}, será alterado para {}, com o aumento de 15%.'.format(salario, salario + salario * 0.15))
