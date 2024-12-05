ce90b44c30766214836650fe31ed7727d4206078
GarciaRPedro <pedroxzz2003@gmail.com>
2024-12-04 22:23:14 -0300

    Ex

---
+++ ex016.py
@@ -0,0 +1,9 @@
+#
+#
+#                   Algoritmo que leia um numero real exiba na tela
+#                                 a sua porção inteira
+#
+#
+import  math
+num = float(input('Digite um numero Real: '))
+print('A parte inteira de {} é {}'.format(num, math.floor(num)))
