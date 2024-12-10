f5de7e79c086b5f833ad75fe9e9a344ed94f72d7
GarciaRPedro <pedroxzz2003@gmail.com>
2024-12-10 10:11:03 -0300

    Update ex019.py

--- ex019.py
+++ ex019.py
@@ -1,6 +1,16 @@
 #
 #
-#                   Algoritmo que leia um angulo qualquer e mostre na tela
-#                      O valor do seno cosseno e tangente desse angulo
+#                   Algoritmo que leia o nome de quatro alunos e exiba na tela
+#                                aleatoriamente o nome de um deles
 #
 #
+import math
+import random
+
+name1 = str(input('Digite o nome do primero aluno(a): '))
+name2 = str(input('Digite o nome do segundo aluno(a): '))
+name3 = str(input('Digite o nome do terceiro aluno(a): '))
+name4 = str(input('Digite o nome do quarto aluno(a): '))
+alunos = (name1, name2, name3, name4)
+escolhido = (random.choice(alunos))
+print ('O aluno escolhido para apagar o quadro foi o: {}'.format(escolhido))
