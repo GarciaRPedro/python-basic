169f7767869a179d1a034444b30f981539c1909e
GarciaRPedro <pedroxzz2003@gmail.com>
2025-05-19 11:14:00 -0300

    Update aula09.py

--- aula09.py
+++ aula09.py
@@ -1,2 +1,4 @@
-frase = 'string'
-print(len(frase))
+"""ex. 22 crie um programa que leia o nome completo de uma pessoa e mostre :
+-O nome com todas as letras maiúsculas
+-Quantas letras tem ao todo (sem considerar espacos
+- quantas letras tem o primeito nome""" 
