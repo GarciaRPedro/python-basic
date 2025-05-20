d0cbc5e7bc70299a89e4631f771494032949b988
GarciaRPedro <pedroxzz2003@gmail.com>
2025-05-20 18:41:50 -0300

    Update aula09.py

--- aula09.py
+++ aula09.py
@@ -32,6 +32,9 @@ com todas as palavras de uma cadeia de caracteres
 
 """
 frase = 'Curso em Video Python'
+print(frase[1:15:2])
+
+
 #ex. 22 crie um programa que leia o nome completo de uma pessoa e mostre :
 #- O nome com todas as letras maiúsculas
 #- Quantas letras tem ao todo (sem considerar espaços)
