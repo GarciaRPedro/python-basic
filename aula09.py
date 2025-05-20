513c157743305665cacf6304a7cb1765224dda0f
GarciaRPedro <pedroxzz2003@gmail.com>
2025-05-19 22:28:38 -0300

    Update aula09.py

--- aula09.py
+++ aula09.py
@@ -1,4 +1,18 @@
-"""ex. 22 crie um programa que leia o nome completo de uma pessoa e mostre :
--O nome com todas as letras maiúsculas
--Quantas letras tem ao todo (sem considerar espacos
-- quantas letras tem o primeito nome""" 
+"""
+len(frase) = exibe informações sobre a frase
+
+frase.count('o') = conta quantas vezes o caractere entre aspas aparece na frase
+
+frase.find('deo') = Exibe em que parte foi iniciada o conjunto de caracteres 'deo'
+
+'Curso' in frase = exibe se a palavra existe dentro da frase 
+
+ex. 22 crie um programa que leia o nome completo de uma pessoa e mostre :
+- O nome com todas as letras maiúsculas
+- Quantas letras tem ao todo (sem considerar espaços)
+- Quantas letras tem o primeiro nome
+-
+-
+-
+
+"""
