692341bd24c6856afd97a529dd2319fe4d467dad
GarciaRPedro <131564556+GarciaRPedro@users.noreply.github.com>
2025-05-31 11:40:12 -0300

    Update ex022.py

--- ex022.py
+++ ex022.py
@@ -1,5 +1,17 @@
 #
+#   ex. 22 crie um programa que leia o nome completo de uma pessoa e mostre :
+#- O nome com todas as letras maiúsculas
+#- Quantas letras tem ao todo (sem considerar espaços)
+#- Quantas letras tem o primeiro nome
 #
 #
-#
-#
+
+nome = str(input('Digite seu nome\n')) 
+
+print("O seu nome com todas as letras maiusculas é:\n", nome.upper())
+
+print("O seu nome com todas as letras minusculas é:\n", nome.lower())
+
+print(nome.split())
+
+print(''.join(nome))
